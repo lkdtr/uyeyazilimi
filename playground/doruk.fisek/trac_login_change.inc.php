a2ca23633aabb8b6b30b2d5b943a1db76733ef62
doruk.fisek <doruk.fisek@591c5e07-2753-0410-828e-892ff15f6c3a>
2009-05-16 17:35:52 +0000

    cc alanlarinda degisiklik disinda tamam gibi, o biraz daha ugrastiracak -- virgulle ayrilmis bircok adres bulunduran text cunku cc alanlari

---
+++ playground/doruk.fisek/trac_login_change.inc.php
@@ -0,0 +1,29 @@
+<?php
+
+ function trac_login_change ($oldlogin, $newlogin)
+  {
+   // This simple function creates a query to change the login name of a person in the Trac 0.11.x database
+   // TODO : Changing the cc fields -- ticket.cc || WHERE ticket_change.field = cc
+
+   // ticket_change require special attention
+   $query = 'UPDATE ticket_change SET oldvalue = "' . $newlogin . '" WHERE field = "owner" AND oldvalue = "' . $oldlogin . '";';
+   $query .= 'UPDATE ticket_change SET newvalue = "' . $newlogin . '" WHERE field = "owner" AND newvalue = "' . $oldlogin . '";';
+
+   // The rest
+   $trac_affected_tables = array ( array ('attachment', 'author'),
+                                   array ('component', 'owner'),
+                                   array ('permission', 'username'),
+                                   array ('revision', 'author'),
+                                   array ('session', 'sid'),
+                                   array ('session_attribute', 'sid'),
+                                   array ('ticket', 'owner'),
+                                   array ('ticket', 'reporter'),
+                                   array ('ticket_change', 'author'),
+                                   array ('wiki', 'author') );
+   foreach ($trac_affected_tables as $table)
+    $query .= 'UPDATE ' . $table[0] . ' SET ' . $table[1] . ' = "' . $newlogin . '" WHERE ' . $table[1] . ' = "' . $oldlogin . '";';
+
+   return $query;
+  }
+
+?>
