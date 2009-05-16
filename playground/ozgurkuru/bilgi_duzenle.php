531c28fe86cc45ba81026f0703d2fe37f35c19f5
ozgur.kuru <ozgur.kuru@591c5e07-2753-0410-828e-892ff15f6c3a>
2009-05-16 15:44:35 +0000

    strip_tag eklendi

--- playground/ozgurkuru/bilgi_duzenle.php
+++ playground/ozgurkuru/bilgi_duzenle.php
@@ -5,16 +5,16 @@
 
 $slug = $_SERVER['PHP_AUTH_USER'];
 
-$query='UPDATE uyeler SET eposta1 = "' . $_POST['txt_mail1'] . '", 
-                          eposta2 = "' . $_POST['txt_mail2'] .'",
-                          Telefon1 = "' . $_POST['txt_telefon1'] .'",
-                          Telefon2 = "' . $_POST['txt_telefon2'] .'",
-                          sehir = "' . $_POST['txt_sehir'] .'",
-                          liste_uyeligi = "' . $_POST['txt_liste'] .'",
-                          gonullu = "' . $_POST['txt_gonullu'] .'",
-                          kimlik_gizli = "' . $_POST['txt_kimlik'] .'",
-                          oylama = "' . $_POST['txt_oylama'] .'",
-                          trac_listesi = "' . $_POST['txt_trac'] .'"
+$query='UPDATE uyeler SET eposta1 = "' . @strip_tags($_POST['txt_mail1']) . '",
+                          eposta2 = "' . @strip_tags($_POST['txt_mail2']) .'",
+                          Telefon1 = "' . @strip_tags($_POST['txt_telefon1']) .'",
+                          Telefon2 = "' . @strip_tags($_POST['txt_telefon2']) .'",
+                          sehir = "' . @strip_tags($_POST['txt_sehir']) .'",
+                          liste_uyeligi = "' . @strip_tags($_POST['txt_liste']) .'",
+                          gonullu = "' . @strip_tags($_POST['txt_gonullu']) .'",
+                          kimlik_gizli = "' . @strip_tags($_POST['txt_kimlik']) .'",
+                          oylama = "' . @strip_tags($_POST['txt_oylama']) .'",
+                          trac_listesi = "' . @strip_tags($_POST['txt_trac']) .'"
         WHERE alias="' . $slug . '@linux.org.tr" ';
 $result = mysql_query($query);
 if($result)
--- playground/ozgurkuru/sifre.php
+++ playground/ozgurkuru/sifre.php
@@ -4,8 +4,8 @@
 @mysql_select_db(DB);
 $slug = $_SERVER['PHP_AUTH_USER'];
 /* üye bilgileri */
-$new_pass1 = $_POST['txt_parola1'];
-$new_pass2 = $_POST['txt_parola2'];
+$new_pass1 = @strip_tags($_POST['txt_parola1']);
+$new_pass2 = @strip_tags($_POST['txt_parola2']);
 if($new_pass1 == $new_pass2)
 {
 $new_pass = md5($new_pass1);
