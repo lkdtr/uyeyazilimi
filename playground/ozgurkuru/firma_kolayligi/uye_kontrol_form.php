9838b182d4512678fb0ef09e0f47f21001315acb
ozgur.kuru <ozgur.kuru@591c5e07-2753-0410-828e-892ff15f6c3a>
2009-05-20 14:33:44 +0000

    uyelere kolaylik saglayacak firmalar icin web arayuzu calismalari

---
+++ playground/ozgurkuru/firma_kolayligi/uye_kontrol_form.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+    <form action='uye_ara.php' method='post'>
+    <table>
+        <tr>
+            <td>Üye Numarası</td>
+            <td><input type='text' name='txt_uyeno'/></td>
+        </tr>
+        <tr>
+            <td>Adının bir harfi:</td>
+            <td><input type='text' name='txt_ad_harf'/></td>
+        </tr>
+        <tr>
+            <td>Soyadının bir harfi:</td>
+            <td><input type='text' name='txt_soyad_harf'/></td>
+        </tr>
+        <tr>
+            <td colspan='2'><input type='submit' value='Ara'/></td>
+        </tr>
+    </table>
+    </form>
+    </body>
+</html>
