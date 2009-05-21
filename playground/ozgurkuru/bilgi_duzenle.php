181536548018eb536f8c28f5963211428781cfa1
ozgur.kuru <ozgur.kuru@591c5e07-2753-0410-828e-892ff15f6c3a>
2009-05-21 08:27:31 +0000

    degiskenler espace edildi

--- playground/ozgurkuru/bilgi_duzenle.php
+++ playground/ozgurkuru/bilgi_duzenle.php
@@ -5,16 +5,16 @@
 
 $slug = $_SERVER['PHP_AUTH_USER'];
 
-$query='UPDATE uyeler SET eposta1 = "' . @strip_tags($_POST['txt_mail1']) . '",
-                          eposta2 = "' . @strip_tags($_POST['txt_mail2']) .'",
-                          Telefon1 = "' . @strip_tags($_POST['txt_telefon1']) .'",
-                          Telefon2 = "' . @strip_tags($_POST['txt_telefon2']) .'",
-                          sehir = "' . @strip_tags($_POST['txt_sehir']) .'",
-                          liste_uyeligi = "' . @strip_tags($_POST['txt_liste']) .'",
-                          gonullu = "' . @strip_tags($_POST['txt_gonullu']) .'",
-                          kimlik_gizli = "' . @strip_tags($_POST['txt_kimlik']) .'",
-                          oylama = "' . @strip_tags($_POST['txt_oylama']) .'",
-                          trac_listesi = "' . @strip_tags($_POST['txt_trac']) .'"
+$query='UPDATE uyeler SET eposta1 = "' . mysql_real_espace_string(@strip_tags($_POST['txt_mail1'])) . '",
+                          eposta2 = "' . mysql_real_espace_string(@strip_tags($_POST['txt_mail2'])) .'",
+                          Telefon1 = "' . mysql_real_espace_string(@strip_tags($_POST['txt_telefon1'])) .'",
+                          Telefon2 = "' . mysql_real_espace_string(@strip_tags($_POST['txt_telefon2'])) .'",
+                          sehir = "' . mysql_real_espace_string(@strip_tags($_POST['txt_sehir'])) .'",
+                          liste_uyeligi = "' . mysql_real_espace_string(@strip_tags($_POST['txt_liste'])) .'",
+                          gonullu = "' . mysql_real_espace_string(@strip_tags($_POST['txt_gonullu'])) .'",
+                          kimlik_gizli = "' . mysql_real_espace_string(@strip_tags($_POST['txt_kimlik'])) .'",
+                          oylama = "' . mysql_real_espace_string(@strip_tags($_POST['txt_oylama'])) .'",
+                          trac_listesi = "' . mysql_real_espace_string(@strip_tags($_POST['txt_trac'])) .'"
         WHERE alias="' . $slug . '@linux.org.tr" ';
 $result = mysql_query($query);
 if($result)
