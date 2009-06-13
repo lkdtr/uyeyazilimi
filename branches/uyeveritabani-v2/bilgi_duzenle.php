9a92e6461b4a2f23094334dc12e76426a8007440
ozgur.kuru <ozgur.kuru@591c5e07-2753-0410-828e-892ff15f6c3a>
2009-06-13 09:23:19 +0000

    mysql_error ile hatalar aliniyor

--- branches/uyeveritabani-v2/bilgi_duzenle.php
+++ branches/uyeveritabani-v2/bilgi_duzenle.php
@@ -9,7 +9,7 @@ $baglanti =mysql_connect(HOST,USER,PASS);
 
 $slug = $_SERVER['PHP_AUTH_USER'];
 $uyeBilgi = 'SELECT eposta1,alias FROM uyeler WHERE alias="' .$slug .'@linux.org.tr"';
-$rs = mysql_query($uyeBilgi);
+$rs = mysql_query($uyeBilgi) or die (mysql_error());
 $eskimail= mysql_fetch_assoc($rs);
 
 $updateUye='UPDATE uyeler SET eposta1 = "' . mysql_real_escape_string(@strip_tags($_POST['txt_mail1'])) . '",
@@ -22,17 +22,9 @@ $updateUye='UPDATE uyeler SET eposta1 = "' . mysql_real_escape_string(@strip_tag
                           kimlik_gizli = "' . mysql_real_escape_string(@strip_tags($_POST['txt_kimlik'])) .'",
                           oylama = "' . mysql_real_escape_string(@strip_tags($_POST['txt_oylama'])) .'",
                           trac_listesi = "' . mysql_real_escape_string(@strip_tags($_POST['txt_trac'])) .'"
-        WHERE alias="' . $slug . '@linux.org.tr" ';
+        WHERE alias="' . $slug . '@linux.org.tr"';
 
 $updateMail = 'UPDATE forwardings SET source="'. $slug .'@linux.org.tr" WHERE destination = "'. $eskimail[eposta1].'"';
-$resultMail = mysql_query($updateMail);
+$resultMail = mysql_query($updateMail) or die (mysql_error());
 
-if($resultMail AND $rs)
-{
-    echo "Bilgileriniz değiştirilmiştir";
-}
-else
-{
-    echo "Bilgileriniz değiştirilirken bir hata olmuştur";
-}
 ?>
