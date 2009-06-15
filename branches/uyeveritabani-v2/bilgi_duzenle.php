3d95ee44dec89b737a1b0f228d7c4570b226f049
ozgur.kuru <ozgur.kuru@591c5e07-2753-0410-828e-892ff15f6c3a>
2009-06-15 17:29:42 +0000

    mysql queryler duzenlendi, degiskenler anlasilir hale getirildi

--- branches/uyeveritabani-v2/bilgi_duzenle.php
+++ branches/uyeveritabani-v2/bilgi_duzenle.php
@@ -1,18 +1,18 @@
 <?php
 @require('db.php'); //veri tabanı bilgilerini bu dosyadan çektiğimi varsayıyorum.
-$baglanti =mysql_connect(HOST,USER,PASS);
-@mysql_select_db(DB,$baglanti);
-@mysql_set_charset('utf8',$baglanti);
+$baglanti_uye =mysql_connect(HOST,USER,PASS);
+@mysql_select_db(DB,$baglanti_uye);
+@mysql_set_charset('utf8',$baglanti_uye);
 $baglanti_postfix=mysql_connect(HOST_MAIL,USER_MAIL,PASS_MAIL);
 @mysql_select_db(DB_MAIL,$baglanti_postfix);
 @mysql_set_charset('utf8',$baglanti_postfix);
 
-$slug = $_SERVER['PHP_AUTH_USER'];
-$uyeBilgi = 'SELECT eposta1,alias FROM uyeler WHERE alias="' .$slug .'@linux.org.tr"';
-$rs = mysql_query($uyeBilgi) or die (mysql_error());
-$eskimail= mysql_fetch_assoc($rs);
+$uye = $_SERVER['PHP_AUTH_USER'];
+$uyeBilgi = 'SELECT eposta1,alias FROM uyeler WHERE alias="' .$uye .'@linux.org.tr"';
+$uyeBilgiSorgu = mysql_query($uyeBilgi,$baglanti_uye) or die (mysql_error());
+$eskimail= mysql_fetch_assoc($uyeBilgiSorgu);
 
-$updateUye='UPDATE uyeler SET eposta1 = "' . mysql_real_escape_string(@strip_tags($_POST['txt_mail1'])) . '",
+$uyeGuncelle='UPDATE uyeler SET eposta1 = "' . mysql_real_escape_string(@strip_tags($_POST['txt_mail1'])) . '",
                           eposta2 = "' . mysql_real_escape_string(@strip_tags($_POST['txt_mail2'])) .'",
                           Telefon1 = "' . mysql_real_escape_string(@strip_tags($_POST['txt_telefon1'])) .'",
                           Telefon2 = "' . mysql_real_escape_string(@strip_tags($_POST['txt_telefon2'])) .'",
@@ -22,9 +22,9 @@ $updateUye='UPDATE uyeler SET eposta1 = "' . mysql_real_escape_string(@strip_tag
                           kimlik_gizli = "' . mysql_real_escape_string(@strip_tags($_POST['txt_kimlik'])) .'",
                           oylama = "' . mysql_real_escape_string(@strip_tags($_POST['txt_oylama'])) .'",
                           trac_listesi = "' . mysql_real_escape_string(@strip_tags($_POST['txt_trac'])) .'"
-        WHERE alias="' . $slug . '@linux.org.tr"';
+        WHERE alias="' . $uye . '@linux.org.tr"';
 
-$updateMail = 'UPDATE forwardings SET source="'. $slug .'@linux.org.tr" WHERE destination = "'. $eskimail[eposta1].'"';
-$resultMail = mysql_query($updateMail) or die (mysql_error());
+$mailGuncelle = 'UPDATE forwardings SET source="'. $uye .'@linux.org.tr" WHERE destination = "'. $eskimail[eposta1].'"';
+$mailGuncelleSorgu = mysql_query($mailGuncelle,$baglanti_postfix) or die (mysql_error());
 
 ?>
