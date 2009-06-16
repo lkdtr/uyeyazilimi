7e3a61d4990265826e9737164d2f8d9b3fd130d9
ozgur.kuru <ozgur.kuru@591c5e07-2753-0410-828e-892ff15f6c3a>
2009-06-16 16:03:09 +0000

    tablo arka planlari uye_bilgi.php ile ayni yapildi

--- branches/uyeveritabani-v2/form.php
+++ branches/uyeveritabani-v2/form.php
@@ -105,36 +105,36 @@ durumlar();
     <table>
         <form action='bilgi_duzenle.php' method="post">
         <tr>
-            <td>Üye Numarası:</td>
-            <td><?php echo $user_info['uye_id']; ?></td>
+            <td bgcolor="#466176">Üye Numarası:</td>
+            <td bgcolor="#F5F5F5"><?php echo $user_info['uye_id']; ?></td>
         </tr>
         <tr>
-            <td>Adı Soyadı:</td>
-            <td><?php echo $user_info['uye_ad'] . $user_info['uye_soyad']; ?></td>
+            <td bgcolor="#466176">Adı Soyadı:</td>
+            <td bgcolor="#F5F5F5"><?php echo $user_info['uye_ad'] . $user_info['uye_soyad']; ?></td>
         </tr>
         <tr>
-            <td>E-Posta Adresi 1:</td>
-            <td><input type='text' name='txt_mail1' value='<?php echo $user_info['eposta1']; ?>'/></td>
+            <td bgcolor="#466176">E-Posta Adresi 1:</td>
+            <td bgcolor="#F5F5F5"><input type='text' name='txt_mail1' value='<?php echo $user_info['eposta1']; ?>'/></td>
         </tr>
         <tr>
-            <td>E-Posta Adresi 2:</td>
-            <td><input type='text' name='txt_mail2' value='<?php echo $user_info['eposta2']; ?>'/></td>
+            <td bgcolor="#466176">E-Posta Adresi 2:</td>
+            <td bgcolor="#F5F5F5"><input type='text' name='txt_mail2' value='<?php echo $user_info['eposta2']; ?>'/></td>
         </tr>
         <tr>
-            <td>Telefon 1:</td>
-            <td><input type='text' name='txt_telefon1' value='<?php echo $user_info['Telefon1']; ?>'/></td>
+            <td bgcolor="#466176">Telefon 1:</td>
+            <td bgcolor="#F5F5F5"><input type='text' name='txt_telefon1' value='<?php echo $user_info['Telefon1']; ?>'/></td>
         </tr>
         <tr>
-            <td>Telefon 2:</td>
-            <td><input type='text' name='txt_telefon2' value='<?php echo $user_info['Telefon2']; ?>'/></td>
+            <td bgcolor="#466176">Telefon 2:</td>
+            <td bgcolor="#F5F5F5"><input type='text' name='txt_telefon2' value='<?php echo $user_info['Telefon2']; ?>'/></td>
         </tr>
         <tr>
-            <td>Yaşadığı Şehir:</td>
-            <td><input type='text' name='txt_sehir' value='<?php echo $user_info['sehir']; ?>'/></td>
+            <td bgcolor="#466176">Yaşadığı Şehir:</td>
+            <td bgcolor="#F5F5F5"><input type='text' name='txt_sehir' value='<?php echo $user_info['sehir']; ?>'/></td>
         </tr>
         <tr>
-            <td>LKD Üye Listesi: Şuanki durumunuz <?php echo $liste_durum; ?></td>
-            <td>
+            <td bgcolor="#466176">LKD Üye Listesi: Şuanki durumunuz <?php echo $liste_durum; ?></td>
+            <td bgcolor="#F5F5F5">
                 <select name='txt_liste'>
                     <option value='1' <?php echo $lo1;?>>Kayıt Olmak İstiyorum</option>
                     <option value='0' <?php echo $lo2;?>>Kayıt Olmak İstemiyorum</option>
@@ -142,8 +142,8 @@ durumlar();
             </td>
         </tr>
         <tr>
-            <td>Gönüllü Çalışmalar(LKD çalışmalarına gönüllü olmak istermisiniz): Şuanki durumunuz <?php echo $gonullu_durum; ?></td>
-            <td>
+            <td bgcolor="#466176">Gönüllü Çalışmalar(LKD çalışmalarına gönüllü olmak istermisiniz): Şuanki durumunuz <?php echo $gonullu_durum; ?></td>
+            <td bgcolor="#F5F5F5">
                 <select name='txt_gonullu'>
                     <option value='1' <?php echo $go1;?>>Gönüllü Olmak İstiyorum</option>
                     <option value='0' <?php echo $go2;?>>Gönüllü Olmak İstemiyorum</option>
@@ -151,8 +151,8 @@ durumlar();
             </td>
         </tr>
         <tr>
-            <td>Elektronik Oylamalar(Elektronik oylamalara katılmak istermisiniz): Şuanki durumunuz <?php echo $oylama_durum; ?></td>
-            <td>
+            <td bgcolor="#466176">Elektronik Oylamalar(Elektronik oylamalara katılmak istermisiniz): Şuanki durumunuz <?php echo $oylama_durum; ?></td>
+            <td bgcolor="#F5F5F5">
                 <select name='txt_oylama'>
                     <option value='1' <?php echo $oo1;?>>Katılmak İstiyorum</option>
                     <option value='0' <?php echo $oo2;?>>Katılmak İstemiyorum</option>
@@ -160,8 +160,8 @@ durumlar();
             </td>
         </tr>
         <tr>
-            <td>Trac Liste Kaydı (Trac mail listesine kayıt durumunuz DİKKAT YÜKSEK TRAFİK): Şuanki durumunuz <?php echo $trac_durum; ?></td>
-            <td>
+            <td bgcolor="#466176">Trac Liste Kaydı (Trac mail listesine kayıt durumunuz DİKKAT YÜKSEK TRAFİK): Şuanki durumunuz <?php echo $trac_durum; ?></td>
+            <td bgcolor="#F5F5F5">
                 <select name='txt_trac'>
                     <option value='1' <?php echo $to1;?>>Kayıt Olmak İstiyorum</option>
                     <option value='0' <?php echo $to2;?>>Kayıt Olmak İstemiyorum</option>
@@ -169,8 +169,8 @@ durumlar();
             </td>
         </tr>
         <tr>
-            <td>Kimlik Gizliliği(Kimliğiniz diğer üyeler tarafından görülebilsin mi?): Şuanki durum <?php echo $kimlik_durum; ?></td>
-            <td>
+            <td bgcolor="#466176">Kimlik Gizliliği(Kimliğiniz diğer üyeler tarafından görülebilsin mi?): Şuanki durum <?php echo $kimlik_durum; ?></td>
+            <td bgcolor="#F5F5F5">
                 <select name='txt_kimlik'>
                     <option value='1' <?php echo $ko1;?>>Evet</option>
                     <option value='0' <?php echo $ko2;?>>Hayır</option>
@@ -178,7 +178,7 @@ durumlar();
             </td>
         </tr>
         <tr>
-            <td colspan='2'><input type='submit' value='Değiştir'/></td>
+            <td bgcolor="#466176" colspan='2'><input type='submit' value='Değiştir'/></td>
         </tr>
         </form>
         <form action='sifre.php' method='post'>
