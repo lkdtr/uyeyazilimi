a183c861b90a73a932a8698b7f0d34c61996a042
bahri.meric.canli <bahri.meric.canli@591c5e07-2753-0410-828e-892ff15f6c3a>
2009-06-17 21:01:10 +0000

    minik rutus

--- branches/uyeveritabani-v2/scripts/uye_bilgi_parola_degistir.php
+++ branches/uyeveritabani-v2/scripts/uye_bilgi_parola_degistir.php
@@ -1,8 +1,9 @@
 <?php
 $uye = $_SERVER['PHP_AUTH_USER'];
 
-$yeni_sifre1 = mysql_real_escape_string(@strip_tags($_POST['txt_parola1']));
-$yeni_sifre2 = mysql_real_escape_string(@strip_tags($_POST['txt_parola2']));
+$yeni_sifre1 = @strip_tags($_POST['txt_parola1']);
+$yeni_sifre2 = @strip_tags($_POST['txt_parola2']);
+
 $yeni_sifre = md5($yeni_sifre1);
 
 if($yeni_sifre1 == $yeni_sifre2)
@@ -14,12 +15,12 @@ if($yeni_sifre1 == $yeni_sifre2)
  // Uye veritabaninda parola degistirelim
  @mysql_select_db(DB,$baglanti_sifre);
  $yeni_sifre = md5($yeni_sifre1);
- $sifre_guncelle='UPDATE uyeler SET PassWord = "' . $yeni_sifre . '" WHERE alias="' . $uye . '@linux.org.tr" ';
+ $sifre_guncelle='UPDATE uyeler SET PassWord = "' . mysql_real_escape_string($yeni_sifre) . '" WHERE alias="' . $uye . '@linux.org.tr" ';
  $sonuc = mysql_query($sifre_guncelle,$baglanti_sifre) or $mesaj = "Parolanız güncellemesinde bir hata oluştu.";
 
  // Bir de yeni uye veritabaninda parolayi guncelleyelim
  @mysql_select_db(DB_PWD,$baglanti_sifre);
- $sifre_guncelle='UPDATE members SET password = "' . $yeni_sifre . '" WHERE lotr_alias="' . $uye . '"';
+ $sifre_guncelle='UPDATE members SET password = "' . mysql_real_escape_string($yeni_sifre) . '" WHERE lotr_alias="' . $uye . '"';
  $sonuc = mysql_query($sifre_guncelle,$baglanti_sifre) or $mesaj = "Parolanız güncellemesinde bir hata oluştu.";
 }
 else
