ad02a244edebc6f7f7395dc29dccde0c44827865
doruk.fisek <doruk.fisek@591c5e07-2753-0410-828e-892ff15f6c3a>
2009-06-17 12:37:35 +0000

    * dondurulen mesaji duzenledim -- hata olusursa bembeyaz sayfada kakalak gibi kalmasin, bununla da ilgi bilgi versin * kod bloklarini duzenledim, araya comment ekledim daha anlasilir hale gelsin diye

--- branches/uyeveritabani-v2/scripts/uye_bilgi_duzenle.php
+++ branches/uyeveritabani-v2/scripts/uye_bilgi_duzenle.php
@@ -1,14 +1,12 @@
 <?php
-@require('db.php'); //veri tabanı bilgilerini bu dosyadan çektiğimi varsayıyorum.
+@require('db.php');    // veritabanı erisim bilgileri
+
+$uye = $_SERVER['PHP_AUTH_USER'];    // kullanici adi
+
+# Uye veritabanina baglanip, guncellemeleri yapalim
 $baglanti_uye =mysql_connect(HOST,USER,PASS);
 @mysql_select_db(DB,$baglanti_uye);
-@mysql_set_charset('utf8',$baglanti_uye);
-$baglanti_postfix=mysql_connect(HOST_MAIL,USER_MAIL,PASS_MAIL);
-@mysql_select_db(DB_MAIL,$baglanti_postfix);
-@mysql_set_charset('utf8',$baglanti_postfix);
-
-$uye = $_SERVER['PHP_AUTH_USER'];
-
+@mysql_query("SET NAMES 'utf8'",$baglanti_uye);
 $uyeGuncelle='UPDATE uyeler SET eposta1 = "' . mysql_real_escape_string(@strip_tags($_POST['txt_mail1'])) . '",
                           eposta2 = "' . mysql_real_escape_string(@strip_tags($_POST['txt_mail2'])) .'",
                           Telefon1 = "' . mysql_real_escape_string(@strip_tags($_POST['txt_telefon1'])) .'",
@@ -23,13 +21,17 @@ $uyeGuncelle='UPDATE uyeler SET eposta1 = "' . mysql_real_escape_string(@strip_t
 $uyeGuncelleSorgu = mysql_query($uyeGuncelle,$baglanti_uye) or die (mysql_error());
 @mysql_close($baglanti_uye);
 
+# E-posta sunucusunda uyenin alias'ini guncelleyelim
+$baglanti_postfix=mysql_connect(HOST_MAIL,USER_MAIL,PASS_MAIL);
+@mysql_select_db(DB_MAIL,$baglanti_postfix);
 $mailGuncelle = 'UPDATE forwardings SET destination = "' . mysql_real_escape_string(@strip_tags($_POST['txt_mail1'])) . '" WHERE source="'. $uye . '@linux.org.tr"';
 $mailGuncelleSorgu = mysql_query($mailGuncelle,$baglanti_postfix) or die (mysql_error());
+mysql_close($baglanti_postfix);
 
-if($mailGuncelleSorgu && $uyeBilgiSorgu)
-{
-	$mesaj = "Güncelleme başarılı.";
-	header("Location: uye_bilgi.php?mesaj=$mesaj"); // yonlendirmeyi bilgi_php ye yapip mesaji orada gosterelim
-}
+if($mailGuncelleSorgu && $uyeGuncelleSorgu)
+ $mesaj = "Bilgileriniz başarıyla güncellendi.";
+else
+ $mesaj = "Bilgi güncellemesinde bir hata oluştu.";
 
+header("Location: uye_bilgi.php?mesaj=$mesaj"); // yonlendirmeyi bilgi_php ye yapip mesaji orada gosterelim
 ?>
