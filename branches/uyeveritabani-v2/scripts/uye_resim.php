72b039888e9bc3a78c58311602350c12a23d4309
tugrul.gurkaynak <tugrul.gurkaynak@591c5e07-2753-0410-828e-892ff15f6c3a>
2009-06-28 09:31:25 +0000

    üye resmi için gösterme betiği

---
+++ branches/uyeveritabani-v2/scripts/uye_resim.php
@@ -0,0 +1,25 @@
+<?php
+
+header("Content-type: image/jpeg");
+
+ $slug = $_SERVER['PHP_AUTH_USER'];    // kullanici adi
+ 
+ // veritabani baglantisi
+ require('db.php');
+ $conn = mysql_connect(HOST, USER, PASS);
+ 
+ mysql_select_db(DB) or die(mysql_error());
+ mysql_query("SET NAMES 'utf8'");
+
+ // uyenin kisisel bilgilerini alalim
+ $query = 'SELECT uye_id FROM uyeler WHERE alias = "' . $slug . '@linux.org.tr"';
+ $result = mysql_query($query);
+ $user_info = mysql_fetch_row($result);
+
+ // veritabanindan alacagimizi aldik
+ mysql_close($conn);
+
+$im = file_get_contents(IMGDIR . '/' . $user_info[0]. '.jpg');
+echo $im;
+?>
+
