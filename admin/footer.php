05554613a63376ee64370def84f4d02aa4383b3c
Doruk Fisek <dfisek@ozguryazilim.com.tr>
2009-07-04 07:36:20 +0000

    tum dosyalara hukmetmek icin tek bir lisans dosyasi ekleyelim, yazar dosyasi zaten eklemistik; tekil dosyalardan el degdikce lisans ve yazar bilgilerini temizleyebiliriz artik

--- admin/footer.php
+++ admin/footer.php
@@ -4,7 +4,7 @@
      <td bgcolor="#EE7B11">
 	 	<table width="100%">
 			<tr>
-			  <td class="navbeyaz" title="(R. Tolga Korkunçkaya tolga@mavibilgisayar.com | Umut Işık umut@forsnet.com.tr)">&nbsp;©2004 LKD &nbsp; Web-CG</td>
+			  <td class="navbeyaz"">&nbsp;LKD 2004</td>
 			  <td class="navbeyaz" align="right">GNU GPL</td>
 			</tr>
 		</table>
