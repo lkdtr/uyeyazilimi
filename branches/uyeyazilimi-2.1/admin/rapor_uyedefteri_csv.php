a638162307848c656f7f4f56fff8fe2de0786f82
doruk.fisek <doruk.fisek@591c5e07-2753-0410-828e-892ff15f6c3a>
2009-07-13 10:30:46 +0000

    * mysql -> utf-8 * Cikan dosyaya ayirt edici isim ekle * Notlar kismi uye defterinde kullanilmiyor, o kismi atalim * Ad, soyad alanlarini birlestir * csv ciktisinin sonundaki html kodunu at

--- branches/uyeyazilimi-2.1/admin/rapor_uyedefteri_csv.php
+++ branches/uyeyazilimi-2.1/admin/rapor_uyedefteri_csv.php
@@ -8,26 +8,25 @@
 		header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
 		header("Cache-Control: private", false); // required for certain browsers 
 		header("Content-Type: text/csv");
-		header("Content-Disposition: attachment; filename=". date("dmY"). ".csv;");
+		header("Content-Disposition: attachment; filename=uyedefteri-". date("dmY"). ".csv;");
 		header("Content-Transfer-Encoding: binary");
 		//header("Content-Length: ".filesize($filename));	
 		
 
         	$Baglanti = @mysql_connect(HOST, USER, PASS) or die("Baglanti kurulamadi");
         	@mysql_select_db(DB) or die("Veritabani secilemedi");
+		mysql_query("SET NAMES 'utf8'");
 
 		$x = intval($_GET["x"]);
 		$y = intval($_GET["y"]);
 	
-       		$Sorgu = "SELECT odemeler.uye_id,uyeler.uye_ad,uyeler.uye_soyad,odemeler.miktar,odemeler.tarih,odemeler.notlar FROM odemeler,uyeler WHERE odemeler.uye_id BETWEEN ". $x ." AND ". $y ." AND odemeler.tur = 'aidat' AND uyeler.uye_id=odemeler.uye_id ORDER BY odemeler.uye_id,odemeler.tarih";
+       		$Sorgu = "SELECT odemeler.uye_id,uyeler.uye_ad,uyeler.uye_soyad,odemeler.miktar,odemeler.tarih FROM odemeler,uyeler WHERE odemeler.uye_id BETWEEN ". $x ." AND ". $y ." AND odemeler.tur = 'aidat' AND uyeler.uye_id=odemeler.uye_id ORDER BY odemeler.uye_id,odemeler.tarih";
        	 	$Sonuc = @mysql_query($Sorgu) or die(mysql_error());
         	mysql_close($Baglanti); 
 		while( $Bilgi = mysql_fetch_array($Sonuc) )
-			echo $Bilgi["uye_id"].";".$Bilgi["uye_ad"].";".$Bilgi["uye_soyad"].";".$Bilgi["miktar"] .";".$Bilgi["tarih"].";".$Bilgi["notlar"]."\n";
+			echo $Bilgi["uye_id"].";".$Bilgi["uye_ad"]." ".$Bilgi["uye_soyad"].";".$Bilgi["miktar"] .";".$Bilgi["tarih"]."\n";
 		mysql_free_result($Sonuc);
 
-		echo " </form>\n";
-		echo "</table>\n";
 	} else {
 		require('header.inc.php');
 		echo "<table align=\"center\" border=\"0\" cellspacing=\"1\" cellpadding=\"10\">\n";
