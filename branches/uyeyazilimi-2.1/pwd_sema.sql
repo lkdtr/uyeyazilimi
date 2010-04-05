4c43358d6acdb4f4d37205b3119866ec9baba498
doruk.fisek <doruk.fisek@591c5e07-2753-0410-828e-892ff15f6c3a>
2010-04-05 19:16:29 +0000

    * Yetkilendirme alani ekle: Sayisal bir yetki seviyesi, ilk etapta Wordpress yetkilendirmeleri icin kullanilacak * E-posta adresini direkt veritabanindan alan uygulamalar var, onlara tek tek "sonuna @linux.org.tr ekle" diye mudahale etmek yerine, e-posta adresini tutalim. * E-posta alanini uye veritabanindan parola veritabanina aktarmak icin betik

--- branches/uyeyazilimi-2.1/pwd_sema.sql
+++ branches/uyeyazilimi-2.1/pwd_sema.sql
@@ -5,7 +5,20 @@ CREATE TABLE IF NOT EXISTS `members` (
   `lastname` varchar(30) collate utf8_turkish_ci default NULL,
   `lotr_alias` varchar(65) collate utf8_turkish_ci default NULL,
   `password` varchar(32) collate utf8_turkish_ci default NULL,
+  `email` varchar(78) collate utf8_turkish_ci default NULL,
+  `privilege` tinyint(3) unsigned default '0',
   PRIMARY KEY  (`id`),
   UNIQUE KEY `members_unique1` (`uye_no`),
-  KEY `members_unique2` (`lotr_alias`)
+  KEY `members_unique3` (`lotr_alias`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci ;
+
+-- --------------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS `password_confirmations` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `member_id` int(10) unsigned NOT NULL default '0',
+  `hash` char(32) collate utf8_turkish_ci default NULL,
+  `created` timestamp NULL default NULL,
+  PRIMARY KEY  (`id`),
+  KEY `password_confirmations_FKIndex1` (`member_id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci ;
