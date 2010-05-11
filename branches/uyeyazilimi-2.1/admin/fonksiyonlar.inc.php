ee7270d08df9551edbc95ca48fcdafc99dc273ef
doruk.fisek <doruk.fisek@591c5e07-2753-0410-828e-892ff15f6c3a>
2010-05-11 17:05:43 +0000

    - Aidat hesaplama kodlarini fonksiyonlara ayir - Aidat raporlarini tek bir raporda birlestir, hem csv hem html cikti verebilsin - Aidat odemelerinin durumuna gore uc farkli tur liste alabilelim (ilk kez aidat odeyen, duzenli aidat odeyen, birikmis aidati olan) - Ayrilan uye listesi veren rapor, uye bolumunde var zaten, oradan bakilabilir, yonetim bolumunden kaldiralim

--- branches/uyeyazilimi-2.1/admin/fonksiyonlar.inc.php
+++ branches/uyeyazilimi-2.1/admin/fonksiyonlar.inc.php
@@ -43,4 +43,77 @@ function ayrilma_bilgilerini_sifirla($uye_no)
     mysql_query($sorgu, $conn) or die(mysql_error());
 }
 
+function toplam_odemesi_gereken_aidat($uye_no)
+{
+    // Duzelt beni: aidat_miktar tablosuna elle her yil aidat miktari eklenmesi gerekiyor. Ayrica gelecek yilin aidati eklenirse de fazladan hesapliyor. Bunun yerine sadece aidat degisimlerini tutmak, bir sonraki yil icin otomatik olarak son aidat miktarini hesaplamak daha iyi olur
+
+    global $conn;
+
+    mysql_select_db(DB,$conn);
+
+    $sorgu = 'SELECT kayit_tarihi FROM uyeler WHERE uye_id = ' . $uye_no;
+    $sonuc = mysql_query($sorgu) or die(mysql_error());
+    $uye = mysql_fetch_array($sonuc);    
+
+    $sorgu = 'SELECT SUM(miktar) FROM aidat_miktar WHERE yil >= '. $uye['kayit_tarihi'];
+    $sonuc = mysql_query($sorgu) or die(mysql_error());
+    $toplam = mysql_fetch_row($sonuc);
+
+    if($uye['kayit_tarihi'] <= 2002)    // 2002 ve oncesinde dernege 5 TL giris ucreti aliniyordu, ileriki yillarda bu uygulama kaldirildi
+        $toplam[0] += 5;
+
+    return $toplam[0];
+}
+
+function toplam_odedigi_aidat($uye_no)
+{
+    global $conn;
+
+    mysql_select_db(DB,$conn);
+    $sorgu = 'SELECT SUM(miktar) FROM odemeler WHERE uye_id = ' . $uye_no;
+    $sonuc = mysql_query($sorgu) or die(mysql_error());
+    $toplam = mysql_fetch_row($sonuc);
+
+    return $toplam[0];
+}
+
+function toplam_aidat_borcu($uye_no)
+{
+    $borc = toplam_odemesi_gereken_aidat($uye_no) - toplam_odedigi_aidat($uye_no);
+
+    if($borc > 0)
+        return $borc;
+    else
+        return 0;
+}
+
+function csv_dosya_http_baslik_bilgilerini_gonder($dosya_adi)
+{
+    header('Pragma: public'); // required
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+    header('Cache-Control: private', false); // required for certain browsers
+    header('Content-Type: text/csv');
+    header('Content-Disposition: attachment; filename=' . $dosya_adi . '_' . date('dmY'). '.csv;');
+    header('Content-Transfer-Encoding: binary');
+}
+
+function eposta2login($lkd_eposta)
+{
+    $parcalar = explode('@', $lkd_eposta);
+
+    return $parcalar[0];
+}
+
+function aidat_miktari($yil)
+{
+    mysql_select_db(DB);
+
+    $sorgu = 'SELECT miktar FROM aidat_miktar WHERE yil = '. $yil;
+    $sonuc = mysql_query($sorgu) or die(mysql_error());
+    $miktar = mysql_fetch_row($sonuc);
+
+    return $miktar[0];
+}
+
 ?>
---
+++ branches/uyeyazilimi-2.1/admin/rapor_aidat.php
@@ -0,0 +1,106 @@
+<?php
+/*
+    Aidat odemelerinin dokumunu csv ya da html biceminde verir.
+
+    Parametrelerin alabilecekleri degerler:
+        * bicem: html (ontanimli), csv
+        * liste: tum (ontanimli), tum_odemesi_bulunan, duzenli_aidat, ilk_aidat, birikmis_aidat
+            + tum: Aidat odemesi olsun ya da olmasin tum uyeler
+            + tum_odemesi_bulunan: Yalniz aidat odemesi bulunan tum uyeler
+            + duzenli_aidat: Yalniz bulunulan yil disinda aidat odemesi bulunmayan ve iki seneden uzun suredir uye olan
+            + ilk_aidat: Yalniz bulunulan yil disinda aidat odemesi bulunmayan ve iki seneden kisa suredir uye olan (yani kayit olduktan sonra ilk kez aidat odeyecek)
+            + birikmis_aidat: Yalniz birden fazla yila ait odemesi bulunan
+*/
+
+    require('ayarlar.inc.php');
+    require('fonksiyonlar.inc.php');
+
+    if($_GET['bicem'] == 'csv')
+    {
+        csv_dosya_http_baslik_bilgilerini_gonder($_GET['liste']);
+	echo "Üye Numarası;Ad Soyad;Kullanıcı Adı;Kayıt Tarihi;Aidat Borcu;\n";
+    }
+    else    // ontanimli html cikti
+    {
+        echo "<html>\n" .
+             ' <head><title>LKD Aidat Ödeme Listesi .:. ' . $_GET['liste'] . "</title>\n" .
+             "  <meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"/>\n" .
+             " </head>\n" .
+             "<body>\n";
+
+        echo "<table bgcolor=\"#D6DDE7\" border=0 bgcolor=\"white\" cellpadding=3 cellspacing=1>\n" .
+             " <tr bgcolor=\"#466176\">\n" .
+             "  <td align=\"left\"><font color=\"#ffffff\">Ad Soyad</font></td>\n" .
+             "  <td align=\"center\"><font color=\"#ffffff\">Üye Numarası</font></td>\n" .
+             "  <td align=\"center\"><font color=\"#ffffff\">Kullanıcı Adı</font></td>\n" .
+             "  <td align=\"center\"><font color=\"#ffffff\">Kayıt Tarihi</font></td>\n" .
+             "  <td align=\"center\"><font color=\"#ffffff\">Aidat Borcu</font></td>\n" .
+             " </tr>\n";
+
+        $Renkler = array("#ffffff", "#f5f5f5"); // Tablonun okunmasi kolaylassin
+    }
+
+    // Veritabaniyla baglanti kurup uyelerin listesini alalim
+    @mysql_connect(HOST, USER, PASS) or die("Bağlanti kurulamadı");
+    @mysql_select_db(DB) or die("Veritabanı seçilemedi");
+    mysql_query("SET NAMES 'utf8'");
+    $sorgu = 'SELECT uye_id,uye_ad,uye_soyad,alias,kayit_tarihi FROM uyeler WHERE artik_uye_degil=0 ORDER BY uye_ad';
+    $sonuc = mysql_query($sorgu) or die(mysql_error());
+
+    $bu_yil = date('Y');
+    $bu_yil_aidati = aidat_miktari($bu_yil);
+
+    while($uye = mysql_fetch_array($sonuc))    // Siradan uyeleri isleyleim
+    {
+        $Renk = $Renkler[ ++$RenkSec%2 ];
+
+        $lkd_login = eposta2login($uye['alias']);
+        $borc = toplam_aidat_borcu($uye['uye_id']);
+        $uyelik_yil_sayisi = $bu_yil - $uye['kayit_tarihi'] + 1;    // uyeliginin kacinci yili
+
+        if($_GET['bicem'] == 'csv')
+            $cikti =  $uye['uye_ad'] . ' ' . $uye['uye_soyad'] . ';' .
+                      $uye['uye_id'] . ';' .
+                      $lkd_login . ';' .
+                      $uye['kayit_tarihi'] . ';' .
+                      $borc . "\n";
+        else
+            $cikti = " <tr bgcolor=$Renk>\n" .
+                     '  <td>' . $uye['uye_ad'] . ' ' . $uye['uye_soyad'] . "</td>\n" .
+                     '  <td>' . $uye['uye_id'] . "</td>\n" .
+                     "  <td>$lkd_login</td>\n" .
+                     '  <td>' . $uye['kayit_tarihi'] . "</td>\n" .
+                     '  <td>' . FormatCurrency($borc, 0, -2, -2, -2) .  "</td>\n" .
+                     ' </tr>';
+
+        switch($_GET['liste'])
+        {
+            case 'tum_odemesi_bulunan':
+                if($borc > 0)
+                    echo $cikti;
+                break;
+            case 'duzenli_aidat':
+                if($borc > 0 && $borc <= $bu_yil_aidati && $uyelik_yil_sayisi > 2)
+                    echo $cikti;
+                break;
+            case 'ilk_aidat':
+                if($borc > 0 && $borc <= $bu_yil_aidati && $uyelik_yil_sayisi == 2)
+                    echo $cikti;
+                break;
+            case 'birikmis_aidat':
+                if($borc > $bu_yil_aidati)
+                    echo $cikti;
+                break;
+            default:
+                echo $cikti;
+                break;
+        }
+
+    }    // hoop basa
+
+    mysql_free_result($Sonuc);
+    mysql_close($Baglanti);
+
+    if($_GET['bicem'] != 'csv')
+        echo "</table>\n</body>\n</html>"
+?>
