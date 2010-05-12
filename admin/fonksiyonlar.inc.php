12c2244df84efbf906f18dc134a1dce305d738a2
Doruk Fisek <dfisek@ozguryazilim.com.tr>
2010-05-12 05:31:51 +0000

    birden fazla veritabani baglantisi kullanilmiyor, su degiskenden kurtulalim

--- admin/fonksiyonlar.inc.php
+++ admin/fonksiyonlar.inc.php
@@ -5,51 +5,41 @@ require('fonksiyonlar_tarih.inc.php');
 
 function eposta_yonlendirmesi_ac($lkd_eposta, $hedef_eposta)
 {
-    global $conn;
-
-    mysql_select_db(DB_MAIL,$conn);
+    mysql_select_db(DB_MAIL);
     $sorgu = 'INSERT INTO forwardings VALUES("' . $lkd_eposta . '","' . $hedef_eposta . '")';
-    mysql_query($sorgu, $conn) or die(mysql_error());
+    mysql_query($sorgu) or die(mysql_error());
 }
 
 function trac_veritabanina_ekle($lkd_login, $ad, $soyad, $lkd_eposta)
 {
-    global $conn;
-
-    mysql_select_db(DB_TRAC,$conn);
+    mysql_select_db(DB_TRAC);
     $sorgu = 'INSERT INTO session VALUES ("' . $lkd_login . '", 1, 0)';
-    mysql_query($sorgu, $conn) or die(mysql_error());
+    mysql_query($sorgu) or die(mysql_error());
     $sorgu = 'INSERT INTO session_attribute VALUES ("' . $lkd_login . '", 1, "name", "' . $ad . ' ' . $soyad . '"),
                                                     ("' . $lkd_login . '", 1, "email", "' . $lkd_eposta . '");';
-    mysql_query($sorgu, $conn) or die(mysql_error());
+    mysql_query($sorgu) or die(mysql_error());
 }
 
 function parola_veritabanina_ekle($uye_no, $lkd_login, $ad, $soyad, $lkd_eposta, $privilege)
 {
-    global $conn;
-
-    mysql_select_db(DB_PWD,$conn);
+    mysql_select_db(DB_PWD);
     $sorgu = 'INSERT INTO members (uye_no,name,lastname,lotr_alias,email,privilege)
                            VALUES  (' . $uye_no . ',"' . $ad . '","' . $soyad . '","' . $lkd_login . '","' . $lkd_eposta . '",' . $privilege . ')';
-    mysql_query($sorgu, $conn) or die(mysql_error());
+    mysql_query($sorgu) or die(mysql_error());
 }
 
 function ayrilma_bilgilerini_sifirla($uye_no)
 {
-    global $conn;
-
-    mysql_select_db(DB,$conn);
+    mysql_select_db(DB);
     $sorgu = 'UPDATE uyeler SET ayrilma_tarihi = "", Ayrilma_karar_no = "", Ayrilma_karar_tarih = "", kayit_kapanis_tarih = NULL WHERE uye_id = ' . $uye_no;
-    mysql_query($sorgu, $conn) or die(mysql_error());
+    mysql_query($sorgu) or die(mysql_error());
 }
 
 function toplam_odemesi_gereken_aidat($uye_no)
 {
     // Duzelt beni: aidat_miktar tablosuna elle her yil aidat miktari eklenmesi gerekiyor. Ayrica gelecek yilin aidati eklenirse de fazladan hesapliyor. Bunun yerine sadece aidat degisimlerini tutmak, bir sonraki yil icin otomatik olarak son aidat miktarini hesaplamak daha iyi olur
 
-    global $conn;
-
-    mysql_select_db(DB,$conn);
+    mysql_select_db(DB);
 
     $sorgu = 'SELECT kayit_tarihi FROM uyeler WHERE uye_id = ' . $uye_no;
     $sonuc = mysql_query($sorgu) or die(mysql_error());
@@ -67,9 +57,7 @@ function toplam_odemesi_gereken_aidat($uye_no)
 
 function toplam_odedigi_aidat($uye_no)
 {
-    global $conn;
-
-    mysql_select_db(DB,$conn);
+    mysql_select_db(DB);
     $sorgu = 'SELECT SUM(miktar) FROM odemeler WHERE uye_id = ' . $uye_no;
     $sonuc = mysql_query($sorgu) or die(mysql_error());
     $toplam = mysql_fetch_row($sonuc);
