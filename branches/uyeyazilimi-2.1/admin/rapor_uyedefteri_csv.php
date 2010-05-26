e73ad47132c95ea11b857da25a6d49bf2abc85af
doruk.fisek <doruk.fisek@591c5e07-2753-0410-828e-892ff15f6c3a>
2010-05-26 16:28:11 +0000

    - uye defteri icin aidat dokumu alirken tarih araligi da belirtilebilsin - ortak csv baslik fonksiyonunu kullan - isimlerin karismamasi icin menude rapor_aidat.php baglantisinin adi "aidat tablosu" olsun

--- branches/uyeyazilimi-2.1/admin/rapor_uyedefteri_csv.php
+++ branches/uyeyazilimi-2.1/admin/rapor_uyedefteri_csv.php
@@ -1,26 +1,19 @@
 <?php
 	require('ayarlar.inc.php');
+	require('fonksiyonlar.inc.php');
 
 	if( isset($_GET["x"]) && isset($_GET["y"]) ) { // Aralik verilmisse csv ciktisini bastir
 
-		header("Pragma: public"); // required
-		header("Expires: 0");
-		header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
-		header("Cache-Control: private", false); // required for certain browsers 
-		header("Content-Type: text/csv");
-		header("Content-Disposition: attachment; filename=uyedefteri-". date("dmY"). ".csv;");
-		header("Content-Transfer-Encoding: binary");
-		//header("Content-Length: ".filesize($filename));	
-		
+                csv_dosya_http_baslik_bilgilerini_gonder('uyedefteri');
 
         	$Baglanti = @mysql_connect(HOST, USER, PASS) or die("Baglanti kurulamadi");
         	@mysql_select_db(DB) or die("Veritabani secilemedi");
 		mysql_query("SET NAMES 'utf8'");
 
-		$x = intval($_GET["x"]);
-		$y = intval($_GET["y"]);
-	
-       		$Sorgu = "SELECT odemeler.uye_id,uyeler.uye_ad,uyeler.uye_soyad,odemeler.miktar,odemeler.tarih FROM odemeler,uyeler WHERE odemeler.uye_id BETWEEN ". $x ." AND ". $y ." AND odemeler.tur = 'aidat' AND uyeler.uye_id=odemeler.uye_id ORDER BY odemeler.uye_id,odemeler.tarih";
+                foreach ($_GET as $key => $value)
+                    $$key = intval($value);
+
+      		$Sorgu = 'SELECT odemeler.uye_id,uyeler.uye_ad,uyeler.uye_soyad,odemeler.miktar,odemeler.tarih FROM odemeler,uyeler WHERE odemeler.uye_id BETWEEN ' . $x . ' AND ' . $y . ' AND odemeler.tur = "aidat" AND uyeler.uye_id = odemeler.uye_id AND odemeler.tarih BETWEEN "' . $ilk_yil . '-' . $ilk_ay . '-' . $ilk_gun . '" AND "' . $son_yil . '-' . $son_ay . '-' . $son_gun . '" ORDER BY odemeler.uye_id,odemeler.tarih';
        	 	$Sonuc = @mysql_query($Sorgu) or die(mysql_error());
         	mysql_close($Baglanti); 
 		while( $Bilgi = mysql_fetch_array($Sonuc) )
@@ -31,12 +24,16 @@
 		require('header.inc.php');
 		echo "<table align=\"center\" border=\"0\" cellspacing=\"1\" cellpadding=\"10\">\n";
 		echo " <tr bgcolor=\"#466176\">\n";
+		echo " <td>&nbsp;</td>\n";
 		echo " <td align=\"center\"><b><font color=\"#ffffff\">Baslangic</font></b></td>\n";
 		echo " <td align=\"center\"><b><font color=\"#ffffff\">Bitis</font></b></td>\n";
 		echo " </tr>\n";
 
 		echo " <tr>\n";
-		echo "  <form method=\"get\" action=\"$_PHP_SELF\">\n";
+		echo "  <form method=\"get\" action=\"$_SERVER[PHP_SELF]\">\n";
+		echo " 	<td align=\"center\" bgcolor=\"#ffffff\">\n";
+		echo "   Üye No <br>\n";
+		echo "  </td>\n";
 		echo " 	<td align=\"center\" bgcolor=\"#ffffff\">\n";
 		echo "   <input name=\"x\" type=\"text\"  size=3> <br>\n";
 		echo "  </td>\n";
@@ -44,9 +41,25 @@
 		echo "   <input name=\"y\" type=\"text\" size=3> <br>\n";
 		echo "  </td>\n";
 		echo " </tr>\n";
+
+		echo " <tr>\n";
+		echo " 	<td align=\"center\" bgcolor=\"#ffffff\">\n";
+		echo "   Tarih <br>\n";
+		echo "  </td>\n";
+		echo " 	<td align=\"center\" bgcolor=\"#ffffff\">\n";
+		echo "   <input name=\"ilk_gun\" type=\"text\"  size=2>\n";
+		echo "   <input name=\"ilk_ay\" type=\"text\"  size=2>\n";
+		echo "   <input name=\"ilk_yil\" type=\"text\"  size=4>\n";
+		echo "  </td>\n";
+		echo " 	<td align=\"center\" bgcolor=\"#ffffff\">\n";
+		echo "   <input name=\"son_gun\" type=\"text\"  size=2>\n";
+		echo "   <input name=\"son_ay\" type=\"text\"  size=2>\n";
+		echo "   <input name=\"son_yil\" type=\"text\"  size=4>\n";
+		echo "  </td>\n";
+		echo " </tr>\n";
 		
 		echo " <tr>\n";
-		echo "  <td colspan=2 align=\"center\" bgcolor=\"#ffffff\">\n";
+		echo "  <td colspan=3 align=\"center\" bgcolor=\"#ffffff\">\n";
 		echo "   <input type=\"submit\" value=\"Gonder\n";
 		echo "  </td>\n";
 		echo " </tr>\n";
